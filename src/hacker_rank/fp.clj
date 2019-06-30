e89b80a745fff95703b2867359871664f2f0bf1e
zrma <bulbitain@gmail.com>
2019-06-30 09:11:45 +0900

    Change setting and do reformatting.

--- src/hacker_rank/fp.clj
+++ src/hacker_rank/fp.clj
@@ -1,11 +1,11 @@
 (ns hacker-rank.fp)
 
 (defn solve-me-first [x y]
-	(+ x y))
+  (+ x y))
 
 (defn hello-world []
-	(print "Hello World"))
+  (print "Hello World"))
 
 (defn hello-word-n-times [n]
-	(dorun
-		(for [_ (range n)] (println "Hello World"))))
+  (dorun
+    (for [_ (range n)] (println "Hello World"))))
--- test/hacker_rank/fp_test.clj
+++ test/hacker_rank/fp_test.clj
@@ -1,21 +1,21 @@
 (ns hacker-rank.fp-test
-	(:require [clojure.test :refer :all]
-			  [hacker-rank.fp :refer :all]))
+  (:require [clojure.test :refer :all]
+            [hacker-rank.fp :refer :all]))
 
 
 (deftest hello-world-test
-	(testing ["Hello World"
-			  "https://www.hackerrank.com/challenges/fp-hello-world/problem"])
-	(is (= "Hello World" (with-out-str (hello-world)))))
+  (testing ["Hello World"
+            "https://www.hackerrank.com/challenges/fp-hello-world/problem"])
+  (is (= "Hello World" (with-out-str (hello-world)))))
 
 
 (deftest solve-me-first-test
-	(testing ["Solve Me First FP"
-			  "https://www.hackerrank.com/challenges/fp-solve-me-first/problem"]
-		(is (= (+ 3 5) (solve-me-first 3 5)))))
+  (testing ["Solve Me First FP"
+            "https://www.hackerrank.com/challenges/fp-solve-me-first/problem"]
+    (is (= (+ 3 5) (solve-me-first 3 5)))))
 
 (deftest hello-word-n-times-test
-	(testing ["Hello World N Times"
-			  "https://www.hackerrank.com/challenges/fp-hello-world-n-times/problem"]
-		(is (= (with-out-str (dorun (for [_ (range 3)] (println "Hello World"))))
-			   (with-out-str (hello-word-n-times 3))))))
+  (testing ["Hello World N Times"
+            "https://www.hackerrank.com/challenges/fp-hello-world-n-times/problem"]
+    (is (= (with-out-str (dorun (for [_ (range 3)] (println "Hello World"))))
+           (with-out-str (hello-word-n-times 3))))))
