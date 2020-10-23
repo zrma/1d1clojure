8f6a1fd247575c4047f6766a52552a92e88acc5b
zrma <bulbitain@gmail.com>
2020-10-23 20:27:46 +0900

    update dependencies

--- project.clj
+++ project.clj
@@ -6,10 +6,10 @@
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :repl-options {:init-ns hacker-rank.fp}
   :plugins [
-            [jonase/eastwood "0.3.5"]
-            [lein-kibit "0.1.6"]
+            [jonase/eastwood "0.3.11"]
+            [lein-kibit "0.1.8"]
             [lein-ancient "0.6.15"]
-            [lein-nvd "1.0.0"]
-            [lein-cloverage "1.1.1"]
-            [lein-cljfmt "0.6.4"]
+            [lein-nvd "1.4.1"]
+            [lein-cloverage "1.2.1"]
+            [lein-cljfmt "0.7.0"]
             ])
